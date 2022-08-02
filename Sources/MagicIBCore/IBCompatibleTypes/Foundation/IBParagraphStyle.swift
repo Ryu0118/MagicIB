a9e850e68c26ed82aaec9d09c6166ac9bdc7f5f1
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-08-02 14:39:54 +0900

    fix: Deleted duplicate definitions

--- Sources/MagicIBCore/IBCompatibleTypes/Foundation/IBParagraphStyle.swift
+++ Sources/MagicIBCore/IBCompatibleTypes/Foundation/IBParagraphStyle.swift
@@ -7,32 +7,6 @@
 
 import Foundation
 
-protocol IBCompatibleObject {
-    var properties: [IBPropertyMapper] { get }
-    var functions: [IBFunctionMapper] { get }
-    func mapping(_ attributes: [String: String])
-}
-
-extension IBCompatibleObject {
-    var functions: [IBFunctionMapper] { [] }
-    func mapping(attributes: [String: String]) {
-        attributes.forEach { key, value in
-            properties
-                .filter { $0.ib == key }
-                .forEach { $0.addValue(value) }
-            functions
-                .filter { $0.ib == key }
-                .forEach {
-                    if $0.ib.contains("vertical") || $0.ib.contains("horizontal") {
-                        let axis = $0.ib.contains("vertical") ? "vertical" : "horizontal"
-                        $0.putValueToArgument("init(rawValue: \(value)", type: .enum, at: 0)
-                        $0.putValueToArgument(axis, type: .enum, at: 1)
-                    }
-                }
-        }
-    }
-}
-
 struct IBParagraphStyle: IBCompatibleObject {
     
     var properties: [IBPropertyMapper] {
