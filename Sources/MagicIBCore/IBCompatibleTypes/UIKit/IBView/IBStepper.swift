9b71d58b7667c471ad5ef6c3b4433fdf636cf2dc
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-08-07 01:44:01 +0900

    add: added IBStepper

---
+++ Sources/MagicIBCore/IBCompatibleTypes/UIKit/IBView/IBStepper.swift
@@ -0,0 +1,26 @@
+//
+//  IBStepper.swift
+//  
+//
+//  Created by Ryu on 2022/08/07.
+//
+
+import Foundation
+
+class IBStepper: IBView {
+    private let stepperProperties: [IBPropertyMapper] = [
+        .init(propertyName: "showsMenuAsPrimaryAction", type: .bool),
+        .init(ib: "selected", propertyName: "isSelected", type: .bool),
+        .init(propertyName: "contentHorizontalAlignment", type: .enum),
+        .init(propertyName: "contentVerticalAlignment", type: .enum),
+        .init(propertyName: "wraps", type: .bool),
+        .init(propertyName: "value", type: .number),
+        .init(propertyName: "minimumValue", type: .number),
+        .init(propertyName: "maximumValue", type: .number),
+        .init(propertyName: "stepValue", type: .number),
+    ]
+    
+    override var properties: [IBPropertyMapper] {
+        super.properties + stepperProperties
+    }
+}
