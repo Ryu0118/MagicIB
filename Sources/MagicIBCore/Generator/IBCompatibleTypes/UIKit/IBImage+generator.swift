223dd28e9c3e55de7c08d25ef5db3c2cb099b2ee
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-08-08 20:15:02 +0900

    add: added func generateSwiftCode in IBImage

--- Sources/MagicIBCore/Generator/IBCompatibleTypes/UIKit/IBImage+generator.swift
+++ Sources/MagicIBCore/Generator/IBCompatibleTypes/UIKit/IBImage+generator.swift
@@ -16,9 +16,22 @@ extension IBImage: IBSwiftSourceGeneratable {
      */
     func generateSwiftCode() -> String? {
         var swiftCode = ""
-        if let systemName = self.systemName {
-            
+        if let systemName = self.systemName as? String {
+            if let symbolScale = self.symbolScale as? String {
+                swiftCode.addLine("let symbolConfiguration = UIImage.SymbolConfiguration(scale: .\(symbolScale)")
+                swiftCode.addLine("let image = UIImage(systemName: \"\(systemName))\", withConfiguration: symbolConfiguration)")
+            }
+            else {
+                swiftCode.addLine("let image = UIImage(systemName: \"\(systemName))\"")
+            }
         }
+        else if let name = self.name as? String {
+            swiftCode.addLine("let image = UIImage(named: \"\(name)\")")
+        }
+        if let renderingMode = self.renderingMode as? String {
+            swiftCode.addLine("image?.withRenderingMode(.\(renderingMode)")
+        }
+        return swiftCode.isEmpty ? nil : swiftCode
     }
     
 }
