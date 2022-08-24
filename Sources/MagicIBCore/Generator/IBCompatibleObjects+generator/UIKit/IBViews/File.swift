dd115bd3718196f80927051ede62aa15618bcf88
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-08-25 02:24:13 +0900

    add: added IBCollectionView Generator

---
+++ Sources/MagicIBCore/Generator/IBCompatibleObjects+generator/UIKit/IBViews/File.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ryu on 2022/08/25.
+//
+
+import Foundation
+
+extension IBCollectionView {
+    
+    override func generateSwiftCode() -> [Line] {
+        guard let uniqueName = uniqueName else { return [] }
+        return buildLines {
+            let variableName = classType.variableName
+            let className = classType.description
+            Line(variableName: uniqueName, lineType: .declare(isMutating: false, type: className, operand: "{"))
+            Line(variableName: variableName, lineType: .declare(isMutating: false, type: nil, operand: "\(className)()"))
+            generateCustomizablePropertyLines(variableName: variableName, except: ["contentView"])
+            generateBasicTypePropertyLines(variableName: variableName, except: ["dataMode"])
+            generateNonCustomizablePropertyLines(variableName: variableName)
+            Line(relatedVariableName: uniqueName, custom: "}()")
+        }
+    }
+    
+}
