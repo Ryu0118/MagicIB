918f137a1cdb104820de693ecad60622d7b28fc5
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-08-28 23:04:36 +0900

    clean: refactor

--- Sources/MagicIBCore/Generator/IBCompatibleObjects+generator/Foundation/IBAttributedString+generator.swift
+++ Sources/MagicIBCore/Generator/IBCompatibleObjects+generator/Foundation/IBAttributedString+generator.swift
@@ -54,69 +54,84 @@ extension IBAttributedString.Fragment: SwiftCodeGeneratable {
         else { return [] }
         switch mode {
         case .modern:
-                /*
-                 var attributedString = AttributedString(str)
-                 
-                 if let range = attributedString.range(of: "string") {
-                     attributedString[range].foregroundColor = .red
-                     attributedString[range].font = .systemFont(ofSize: 12)
-                 }
-                 */
-            return buildLines {
-                Line(relatedVariableName: variableName, custom: "if let range = \(variableName).range(of: \"\(content)\") {")
-                generateCustomizablePropertyLines(variableName: "\(variableName)[range]")
-                generateNonCustomizablePropertyLines(variableName: "\(variableName)[range]")
-                Line.end.changeVariableName(variableName)
-            }
+            /*
+             var attributedString = AttributedString(str)
+             
+             if let range = attributedString.range(of: "string") {
+             attributedString[range].foregroundColor = .red
+             attributedString[range].font = .systemFont(ofSize: 12)
+             }
+             */
+            return generateModernAttributes(variableName: variableName, content: content)
         case .legacy(let count):
-            let attributeName = "stringAttributes\(count + 1)"
-            let stringName = "string\(count + 1)"
+            return generateLegacyAttributes(count: count, content: content)
+        }
+    }
+    
+    private func generateModernAttributes(variableName: String, content: String) -> [Line] {
+        buildLines {
+            Line(relatedVariableName: variableName, custom: "if let range = \(variableName).range(of: \"\(content)\") {")
+            generateCustomizablePropertyLines(variableName: "\(variableName)[range]")
+            generateNonCustomizablePropertyLines(variableName: "\(variableName)[range]")
+            Line.end.changeVariableName(variableName)
+        }
+    }
+    
+    private func generateLegacyAttributes(count: Int, content: String) -> [Line] {
+        let attributeName = "stringAttributes\(count + 1)"
+        let stringName = "string\(count + 1)"
+        
+        return buildLines {
+            //let stringAttributes1: [NSAttributedStringKey : Any] = [
+            Line(variableName: attributeName, lineType: .declare(isMutating: false, type: "[NSAttributedString.Key : Any]", operand: "["))
             
-            return buildLines {
-                //let stringAttributes1: [NSAttributedStringKey : Any] = [
-                Line(variableName: attributeName, lineType: .declare(isMutating: false, type: "[NSAttributedString.Key : Any]", operand: "["))
-                
-                //NSMutableParagraphStyle
-                var paragraphStyleLines = [Line]()
-                if let paragraphStyle = self.paragraphStyle as? IBParagraphStyle {
-                    paragraphStyleLines = paragraphStyle.generateSwiftCode()
-                    paragraphStyleLines
+            //NSMutableParagraphStyle
+            var paragraphStyleLines = [Line]()
+            if let paragraphStyle = self.paragraphStyle as? IBParagraphStyle {
+                paragraphStyleLines = paragraphStyle.generateSwiftCode()
+                paragraphStyleLines
+            }
+            /*
+             let stringAttributes1: [NSAttributedStringKey : Any] = [
+             .foregroundColor : UIColor.blue,
+             .font : UIFont.systemFont(ofSize: 24.0),
+             .paragraphStyle: NSParagraphStyle.default
+             ]
+             */
+            generateAttributes(attributeName: attributeName, paragraphStyleLines: paragraphStyleLines)
+            
+            Line(relatedVariableName: attributeName, custom: "]")
+            //let string1 = NSAttributedString(string: "0123", attributes: stringAttributes1)
+            Line(variableName: stringName, lineType: .declare(isMutating: false, operand: "NSAttributedString(string: \"\(content)\", attributes: \(attributeName))"))
+        }
+    }
+    
+    
+    private func generateAttributes(attributeName: String, paragraphStyleLines: [Line]) -> [Line] {
+        buildLines {
+            activatedProperties.compactMap { property in
+                if let nonCustomizable = property.value as? NonCustomizable {
+                    var type: String?
+                    if let _ = property.value as? IBColor {
+                        type = "UIColor"
+                    }
+                    else if let _ = property.value as? IBFont {
+                        type = "UIFont"
+                    }
+                    
+                    guard let type = type,
+                          let firstLine = nonCustomizable.generateSwiftCode().first
+                    else { fatalError("failed to get right operand. propertyName: \(property.propertyName)") }
+                    let rightOperand = firstLine.explicitType(type).originalValue
+                    return Line(relatedVariableName: attributeName, custom: ".\(property.propertyName): \(rightOperand),")
                 }
-                /*
-                 let stringAttributes1: [NSAttributedStringKey : Any] = [
-                     .foregroundColor : UIColor.blue,
-                     .font : UIFont.systemFont(ofSize: 24.0),
-                     .paragraphStyle: NSParagraphStyle.default
-                 ]
-                 */
-                activatedProperties.compactMap { property in
-                    if let nonCustomizable = property.value as? NonCustomizable {
-                        var type: String?
-                        if let _ = property.value as? IBColor {
-                            type = "UIColor"
-                        }
-                        else if let _ = property.value as? IBFont {
-                            type = "UIFont"
-                        }
-                        
-                        guard let type = type,
-                              let firstLine = nonCustomizable.generateSwiftCode().first
-                        else { fatalError("failed to get right operand. propertyName: \(property.propertyName)") }
-                        let rightOperand = firstLine.explicitType(type).originalValue
-                        return Line(relatedVariableName: attributeName, custom: ".\(property.propertyName): \(rightOperand),")
-                    }
-                    else if let _ = property.value as? IBParagraphStyle,
-                            let variableName = paragraphStyleLines.last?.variableName {
-                        return Line(relatedVariableName: attributeName, custom: ".\(property.propertyName): \(variableName),")
-                    }
-                    else {
-                        return nil
-                    }
+                else if let _ = property.value as? IBParagraphStyle,
+                        let variableName = paragraphStyleLines.last?.variableName {
+                    return Line(relatedVariableName: attributeName, custom: ".\(property.propertyName): \(variableName),")
                 }
-                
-                Line(relatedVariableName: attributeName, custom: "]")
-                //let string1 = NSAttributedString(string: "0123", attributes: stringAttributes1)
-                Line(variableName: stringName, lineType: .declare(isMutating: false, operand: "NSAttributedString(string: \"\(content)\", attributes: \(attributeName))"))
+                else {
+                    return nil
+                }
             }
         }
     }
