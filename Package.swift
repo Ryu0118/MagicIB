21ca441b6e80e9e70513d96f6a36fd67602c0e70
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2022-09-08 00:12:38 +0900

    change: change products

--- Package.swift
+++ Package.swift
@@ -11,9 +11,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "MagicIBCore",
-            targets: ["MagicIBCore"]),
+        .executable(name: "MagicIBCLI", targets: ["MagicIBCLI"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
